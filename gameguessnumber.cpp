9e23d6daa138b72705af066e6bb1be8c5e625752
ushakovadiit14 <dielluvv707@gmail.com>
2024-12-03 10:42:35 +0500

    Update gameguessnumber.cpp
    Правки с учетом заголовочного файла

--- gameguessnumber.cpp
+++ gameguessnumber.cpp
@@ -1,38 +1,7 @@
 #include <iostream>
 #include <ctime>
+#include "Header.h"
 using namespace std;
-
-int randomnum(int a, int b)
-{
-    srand((time(0)));
-    int random_number = rand() % (b  - a + 1) + a; // создание случайного числа в заданном диапазоне 
-    return random_number;
-}
-
-void game(int num)
-{
-    int guess, count = 1;
-    cout << "Введите предполагаемое число: ";
-    cin >> guess;
-    while (guess != num)
-    {
-        if (guess < num)
-        {
-            cout << "Загаданное число больше. Попробуйте еще раз." << endl;
-        }
-        else
-        {
-            cout << "Загаданное число меньше. Попробуйте еще раз." << endl;
-        }
-        count++;
-        cin >> guess;
-    }
-    if (guess == num)     //если загаданное число совпадает с введенным, то выводится победная фраза и количество попыток
-    {
-        cout << "Вы угадали!!!" << endl;
-        cout << "Количество попыток: " << count;
-    }
-}
 
 int main() {
     setlocale(LC_ALL, "RU");
