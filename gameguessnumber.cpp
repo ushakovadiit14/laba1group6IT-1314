e38128517281ba03978eca87e0e3246edd7e94f5
NemkinMaximIT13 <f4kalor@gmail.com>
2024-12-03 10:07:31 +0500

    Update gameguessnumber.cpp
    Исправление кода

--- gameguessnumber.cpp
+++ gameguessnumber.cpp
@@ -4,36 +4,38 @@ using namespace std;
 
 int randomnum(int a, int b)
 {
-    srand((time(0))); 
-    int random_number = rand() % a + (b-a); // создание случайного числа в заданном диапазоне 
+    srand((time(0)));
+    int random_number = rand() % (b  - a + 1) + a; // создание случайного числа в заданном диапазоне 
     return random_number;
 }
 
 void game(int num)
 {
-    int guess, count;
+    int guess, count = 1;
     cout << "";
     cin >> guess;
-    count++;
+    while (guess != num)
+    {
+        if (guess < num)
+        {
+            cout << "больше" << endl;
+        }
+        else
+        {
+            cout << "меньше" << endl;
+        }
+        count++;
+        cin >> guess;
+    }
     if (guess == num)     //если загаданное число совпадает с введенным, то выводится победная фраза и количество попыток
     {
         cout << "" << endl;
         cout << "" << count;
     }
-    else    //проверки на то, больше или меньше ли введенное число по сравнению с загаданным.
-    {
-        if (guess < num)
-        {
-            cout << "" << endl;
-        }
-        else
-        {
-            cout << "" << endl;
-        }
-    }
 }
 
-int main(){
+int main() {
+    setlocale(LC_ALL, "RU");
     cout << ""; // правила игры
     int a, b, num;
     cout << "Введите начальное значение диапазона для генерирования числа: "; //начало диапазона
@@ -46,6 +48,7 @@ int main(){
         a = b;
         b = c;
     }
-    int num=randomnum(a, b);
+    num = randomnum(a, b);
     game(num);
+    return 0;
 }
