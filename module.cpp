2345ac7be984afab701fd0b2a800393371521714
EvstafevaVeraIt-14 <evstafevavera28@gmail.com>
2024-12-09 22:23:17 +0500

    Update module.cpp
    Небольшие правки в модуле.

--- module.cpp
+++ module.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <ctime>
+#include <string>
 using namespace std;
 
 int randomnum(int a, int b)
 {
     srand((time(0)));
-    int random_number = rand() % (b  - a + 1) + a; // создание случайного числа в заданном диапазоне 
+    int random_number = rand() % (b - a + 1) + a; // создание случайного числа в заданном диапазоне 
     return random_number;
 }
 
-bool nebukvi (const string &input) {
-    for (char ch : input) {
-        if (!isdigit(ch)) { // Проверяем, является ли символ цифрой
-            cout << "Ошибка: Ввод должен содержать только цифры." << endl;
+bool nebukvi(const string& input) {
+    for (int i = 0; i < input.size(); i++) {
+        if (int(input[i]) > 57 || (int(input[i]) < 48 && int(input[i]) != 45))
+        {
             return false;
         }
     }
@@ -21,12 +22,24 @@ bool nebukvi (const string &input) {
 
 void game(int num)
 {
-    int guess, count = 1;
+    string guess; int count = 1;
     cout << "Введите предполагаемое число: ";
     cin >> guess;
-    while (guess != num)
+    while (!nebukvi(guess))
     {
-        if (guess < num)
+        cout << "Ошибка: Ввод должен содержать только цифры." << endl;
+        cout << "Пожалуйста, введите число(((: "; cin >> guess;
+    }
+    int guess1 = stoi(guess);
+    while (guess != to_string(num))
+    {
+        while (!nebukvi(guess))
+        {
+            cout << "Ошибка: Ввод должен содержать только цифры." << endl;
+            cout << "Пожалуйста, введите число(((: "; cin >> guess;
+        }
+        int guess1 = stoi(guess);
+        if (guess1 < num)
         {
             cout << "Загаданное число больше.Попробуйте ещё раз." << endl;
         }
@@ -37,7 +50,7 @@ void game(int num)
         count++;
         cin >> guess;
     }
-    if (guess == num)     //если загаданное число совпадает с введенным, то выводится победная фраза и количество попыток
+    if (guess == to_string(num))     //если загаданное число совпадает с введенным, то выводится победная фраза и количество попыток
     {
         cout << "Вы выиграли!!!" << endl;
         cout << "Количество попыток: " << count;
