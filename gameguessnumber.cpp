c01d2d81464070f93201bc62c868adde9f836c9b
NemkinMaximIT13 <f4kalor@gmail.com>
2024-12-09 20:19:17 +0500

    Update gameguessnumber.cpp
    Переделка кода с учётом того, что пользователь вводит некорректные данные при задаче концов диапазона для угадывания

--- gameguessnumber.cpp
+++ gameguessnumber.cpp
@@ -11,18 +11,33 @@ int main() {
 "2. Вводим диапазон, в котором компьютер загадает число ;" << endl <<
 "3. Пытаемся угадать заданное число, компьютер будет вам подсказывать;" << endl<<
 "4. В конце видим количество Ваших попыток." << endl; // правила игры
-    int a, b, num;
+    string a, b; int num;
     cout << "Введите начальное значение диапазона для генерирования числа: "; //начало диапазона
     cin >> a;
     cout << "Введите конечное значение диапазона для генерирования числа: "; //конец диапазона
     cin >> b;
-    if (b < a) {
+    if(nebukvi(a) && nebukvi(b)) 
+    {
+        int a1, b1, count=1;
+       for(int i=0;i<a.size();i++)
+       {
+          a1 += int(char(a[i]))*count;
+          count *= 10;
+       }
+       count = 1;
+       for(int i=0;i<b.size();i++)
+       {
+          b1 += int(char(b[i]))*count;
+          count *= 10;
+       }
+       if (b1 < a1) {
         int c;
-        c = a;
-        a = b;
-        b = c;
-    }
-    num = randomnum(a, b);
-    game(num);
-    return 0;
+        c = a1;
+        a1 = b1;
+        b1 = c;
+    } 
+       num = randomnum(a1, b1);
+       game(num);
+       return 0;
+    } 
 }
